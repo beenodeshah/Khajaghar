1e48d6b413393c0152dd0ace75802b31b03cbceb
beenodeshah <binoddshah@gmail.com>
2023-05-28 15:07:23 +0545

    Updated on 28 May-2023

--- userForm.cs
+++ userForm.cs
@@ -15,19 +15,81 @@ namespace Khajaghar
         public userForm()
         {
             InitializeComponent();
+            dgvUserDetails.DataSource = uc.getAllUsers();
         }
-userClass uc=new userClass();
+        userClass uc = new userClass();
+        int userId;
+       
         private void btnCreateUser_Click(object sender, EventArgs e)
         {
             try
             {
-                bool result = uc.manageUsers(0, 
+                bool result = uc.manageUsers(0,
                     txtUserName.Text,
-                    txtPassword.Text, 
+                    txtPassword.Text,
                     cmbRole.Text, 1);
-                if(result==true)
+                if (result == true)
                 {
                     MessageBox.Show("User Successfully Created");
+                    dgvUserDetails.DataSource = uc.getAllUsers();
+                }
+                else
+                {
+                    MessageBox.Show("Error in performing the required operation!");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnEditUser_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool result = uc.manageUsers(userId,
+                    txtUserName.Text,
+                    txtPassword.Text,
+                    cmbRole.Text, 2);
+                if (result == true)
+                {
+                    MessageBox.Show("User Successfully Updated");
+                    dgvUserDetails.DataSource = uc.getAllUsers();
+                }
+                else
+                {
+                    MessageBox.Show("Error in performing the required operation!");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dgvUserDetails_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            userId = int.Parse(dgvUserDetails.SelectedRows[0].Cells["userId"].Value.ToString());
+            txtUserName.Text = dgvUserDetails.SelectedRows[0].Cells["userName"].Value.ToString();
+            txtPassword.Text = dgvUserDetails.SelectedRows[0].Cells["password"].Value.ToString();
+            txtConfirmPassword.Text = txtPassword.Text;
+            cmbRole.Text = dgvUserDetails.SelectedRows[0].Cells["role"].Value.ToString();
+        }
+
+        private void btnDeleteUser_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool result = uc.manageUsers(userId,
+                    txtUserName.Text,
+                    txtPassword.Text,
+                    cmbRole.Text, 3);
+                if (result == true)
+                {
+                    MessageBox.Show("User Successfully Deleted");
                     dgvUserDetails.DataSource = uc.getAllUsers();
                 }
                 else
